40f1968cc735dcf8fdd0da9a5a215dd775425042
Dawid Wydra <ejsik.lel@gmail.com>
2023-07-18 11:35:40 +0200

    the same
    forgot to save 1 file :c

--- gry_telefony/lib/home_page.dart
+++ gry_telefony/lib/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'auth.dart';
+import 'login/auth.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
